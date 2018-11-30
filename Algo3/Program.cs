0d4636a88bc66aa629a07debd4ede9d86dce5182
lkarczewski <karczes.lukasz@gmail.com>
2018-11-30 22:26:05 +0100

    Work in progress... Draws pairs of agents without probability.

--- Algo3/Program.cs
+++ Algo3/Program.cs
@@ -10,18 +10,35 @@ namespace Algo3
     {
         static void Main(string[] args)
         {
-            double[,] test = new double[3, 3] { { 8, -2, 3 }, { 2, 3, -1 }, { 5, -2, 7 } };
-            double[] vector = new double[] { 28.6, 12.12, 36.95 };
-            MyMatrix<double> macierz1 = new MyMatrix<double>(test);
-            double[] vector1 = (dynamic)vector.Clone();
+            //double[,] test = new double[3, 3] { { 8, -2, 3 }, { 2, 3, -1 }, { 5, -2, 7 } };
+            //double[] vector = new double[] { 28.6, 12.12, 36.95 };
+            //MyMatrix<double> macierz1 = new MyMatrix<double>(test);
+            //double[] vector1 = (dynamic)vector.Clone();
 
 
-            macierz1.Jacobi(vector1, 20);
+            //macierz1.Jacobi(vector1, 20);
 
-            for (int i = 0; i < vector1.Length; i++)
-            {
-                Console.WriteLine(vector1[i]);
-            }
+            //for (int i = 0; i < vector1.Length; i++)
+            //{
+            //    Console.WriteLine(vector1[i]);
+            //}
+
+            //double[] op = new double[] { 28.6, 12.12, 36.95, 43.53, 5353.53, 211.11, 99.22 };
+
+            //Random rnd = new Random();
+            //double[] MyRandomArray = op.OrderBy(x => rnd.Next()).ToArray();
+
+            //for (int i = 0; i < MyRandomArray.Length; i++)
+            //{
+            //    Console.Write(MyRandomArray[i]);
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine();
+
+            Test test = new Test();
+
+            test.MonteCarlo(1, 4, 2);
         }
     }
+
 }
--- Algo3/Test.cs
+++ Algo3/Test.cs
@@ -47,5 +47,91 @@ namespace Algo3
             MyMatrix<double> matrix = new MyMatrix<double>(size, size);
             double[] vector = new double[size];
         }
+
+        public void MonteCarlo(int interations, int numOfAgents,int numOfYes)
+        {
+            int[] tableOfAgents = GenerateRandomTableOfAgents(numOfAgents, numOfYes);
+
+            Console.WriteLine("Agents:");
+            for (int i = 0; i < tableOfAgents.Length; i++)
+            {
+                Console.Write(tableOfAgents[i] + " ");
+            }
+
+            Console.WriteLine();
+            List<PairOfAgents> listOfPairs = DrawPairOfAgents(numOfAgents, tableOfAgents);
+
+            Console.WriteLine("Pairs of agents:");
+            foreach (PairOfAgents i in listOfPairs)
+            {
+                Console.WriteLine("First agent: " + i.firstAgent + "\t\tSecond agent: " + i.secondAgent + 
+                    "\t\tFirst index: " + i.firstAgentIndex + "\t\tSecond index: " + i.secondAgentIndex);
+            }
+        }
+
+        public int[] GenerateRandomTableOfAgents(int numberOfAgents, int numberOfYes)
+        {
+            int numberOfNo = numberOfYes / 2;
+            int numberOfUn = numberOfAgents - numberOfNo - numberOfYes;
+            int[] table = new int[numberOfAgents];
+
+            for (int i = 0; i < numberOfYes; i++)
+            {
+                table[i] = 1;
+            }
+
+            for (int i = 0; i < numberOfNo; i++)
+            {
+                table[i + numberOfYes] = -1;
+            }
+
+            for (int i = 0; i < numberOfUn; i++)
+            {
+                table[i + numberOfYes + numberOfNo] = 0;
+            }
+
+            Random rnd = new Random();
+            table = table.OrderBy(x => rnd.Next()).ToArray();
+
+            return table;
+        }
+
+        public struct PairOfAgents
+        {
+            public int firstAgent;
+            public int secondAgent;
+            public int firstAgentIndex;
+            public int secondAgentIndex;
+
+            public PairOfAgents(int fA, int sA, int fAI, int sAI)
+            {
+                firstAgent = fA;
+                secondAgent = sA;
+                firstAgentIndex = fAI;
+                secondAgentIndex = sAI;
+            }
+        }
+
+        public List<PairOfAgents> DrawPairOfAgents(int numberOfAgents, int[] tableOfAgents)
+        {
+            // int[] tableOfAgents = GenerateRandomTableOfAgents(numberOfAgents,18);
+            double probability = (2 / (numberOfAgents * (numberOfAgents - 1)));
+            int[,] tableOfPairs = new int[numberOfAgents/2,4];
+            List<PairOfAgents> listOfPairs = new List<PairOfAgents>();
+            Random rnd = new Random();
+            int k = 0;
+
+            for (int i = 0; i < numberOfAgents; i++)
+            {
+                for (int j = i+1; j < numberOfAgents; j++)
+                {
+                    PairOfAgents n = new PairOfAgents(tableOfAgents[i], tableOfAgents[j], i,  j);
+                    listOfPairs.Add(n);
+                    k++;
+                }
+            }
+
+            return listOfPairs;
+        }
     }
 }
